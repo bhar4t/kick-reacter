15903685df91e993f51d2332b8a7a2f7fa8bf0a2
bhar4t <seizedbharat@gmail.com>
2021-04-18 22:21:33 +0530

    if using async request, creating boiler plate code.

--- src/redux/entity/actions.js
+++ src/redux/entity/actions.js
@@ -7,6 +7,26 @@ const doEntity = () => {
     }
 }
 
+
+// const url = 'https://bhar4t.com/some_api'
+
+// function fetchEntity() {
+//     return function(dispatch) {
+//         // dispatch action if any
+//         fetch(url)
+//         .then(res => res.json())
+//         .then(data => {
+//             // dispatch action if any
+//         })
+//         .catch((e) => {
+//             // dispatch action if any
+//         })
+//     }
+// }
+
+
 export {
     doEntity
 }
+
+// export default fetchEntity
